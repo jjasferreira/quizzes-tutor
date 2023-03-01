25b580bcdd0d645389b4a8e0d30fb1f787ac83cf
Rui Maciel <rui.f.maciel@tecnico.ulisboa.pt>
2023-03-01 15:32:33 +0000

    feat: add class skeleton StudentStats Refs: #21

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/StudentStats.java
@@ -0,0 +1,63 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
+import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
+
+import javax.persistence.*;
+
+
+@Entity
+public class StudentStats implements DomainEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    private CourseExecution courseExecution;
+
+    @ManyToOne
+    private TeacherDashboard teacherDashboard;
+
+    public StudentStats() {
+    }
+
+    public StudentStats(CourseExecution courseExecution, TeacherDashboard teacherDashboard) {
+        setCourseExecution(courseExecution);
+        setTeacherDashboard(teacherDashboard);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public CourseExecution getCourseExecution() {
+        return courseExecution;
+    }
+
+    public void setCourseExecution(CourseExecution courseExecution) {
+        this.courseExecution = courseExecution;
+    }
+
+    public TeacherDashboard getTeacherDashboard() {
+        return teacherDashboard;
+    }
+
+    public void setTeacherDashboard(TeacherDashboard teacherDashboard) {
+        this.teacherDashboard = teacherDashboard;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentStats{" +
+                "id=" + id +
+                ", courseExecution=" + courseExecution +
+                ", teacherDashboard=" + teacherDashboard +
+                '}';
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        // Only used for XML generation
+    }
+}
