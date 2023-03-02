5e9be045662442d2001a7ff24ac1ec08d9e4b40d
Adriana Nunes <adriana.nunes@tecnico.ulisboa.pt>
2023-03-02 17:44:43 +0000

    fix: function to get unique answers Refs: #22

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuestionStats.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuestionStats.java
@@ -1,14 +1,19 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.*;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswer;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer;
 import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
 import pt.ulisboa.tecnico.socialsoftware.tutor.questionsubmission.domain.QuestionSubmission;
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz;
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.QuizQuestion;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
 
 @Entity
@@ -65,11 +70,14 @@ public class QuestionStats implements DomainEntity {
     private int getNumUniqueQuestionsAnswered() {
         Set<Question> uniqueQuestionsAnswered = new HashSet<>();
 
-        for(QuestionSubmission submission : this.execution.getQuestionSubmissions()) {
-            if(!uniqueQuestionsAnswered.contains(submission.getQuestion())) {
-                uniqueQuestionsAnswered.add(submission.getQuestion());
+        for(Quiz quiz : this.execution.getQuizzes()) {
+            for (QuizQuestion quizQuestion : quiz.getQuizQuestions()) {
+                for (QuestionAnswer questionAnswer : quizQuestion.getQuestionAnswers())
+                    if (!uniqueQuestionsAnswered.contains(questionAnswer.getQuestion())) {
+                        uniqueQuestionsAnswered.add(questionAnswer.getQuestion());
+                    }
+                }
             }
-        }
 
         return uniqueQuestionsAnswered.size();
     }
