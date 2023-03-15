b3c64ca55f0b52c9c97f328aba6db9ff4d7ebf75
José João Ferreira <josejoaoferreira@tecnico.ulisboa.pt>
2023-03-15 13:14:11 +0000

    feat(dto): create QuizStatsDto and include them in the TeacherDashboardDto Refs: #64

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+    private List<QuizStatsDto> quizStats;
 
     private List<QuestionStatsDto> questionStats;
 
