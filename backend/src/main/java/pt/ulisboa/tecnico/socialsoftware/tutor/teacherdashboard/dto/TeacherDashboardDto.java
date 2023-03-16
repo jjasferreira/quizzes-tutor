c41b6cc271fee7446a8b3cfc95c558a7fc7d8eb6
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2023-03-16 15:58:57 +0000

    feat: put QuestionStatsDto into TeacherDashboardDto Refs: #66

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -1,18 +1,25 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+    private List<QuestionStatsDto> questionStats;
 
     public TeacherDashboardDto() {
+        this.questionStats = new ArrayList<>();
     }
 
     public TeacherDashboardDto(TeacherDashboard teacherDashboard) {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+        this.questionStats = new ArrayList<>();
+        teacherDashboard.getQuestion().forEach(questionStats -> this.questionStats.add(new QuestionStatsDto(questionStats)));
     }
 
     public Integer getId() {
@@ -31,11 +38,20 @@ public class TeacherDashboardDto {
         this.numberOfStudents = numberOfStudents;
     }
 
+    public List<QuestionStatsDto> getQuestionStats() {
+        return this.questionStats;
+    }
+
+    public void addQuestionStats(QuestionStatsDto questionStats) {
+        this.questionStats.add(questionStats);
+    }
+
     @Override
     public String toString() {
         return "TeacherDashboardDto{" +
                 "id=" + id +
                 ", numberOfStudents=" + this.getNumberOfStudents() +
+                ", questionStats=" + this.getQuestionStats() +
                 "}";
     }
 }
