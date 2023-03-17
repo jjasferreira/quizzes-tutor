ea027ca7295ca2a05dd31c0fc9b7690790a1e463
Rui Maciel <rui.f.maciel@tecnico.ulisboa.pt>
2023-03-17 13:12:47 +0000

    test(backend): Test if student stats are updated when TeacherDashboardService updates Refs: #82

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/TeacherDashboardStudentStatsTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/TeacherDashboardStudentStatsTest.groovy
@@ -8,6 +8,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
 import spock.lang.Unroll
 
@@ -102,6 +103,86 @@ class TeacherDashboardStudentStatsTest extends SpockTest {
         where:
         // We are testing different numbers of course executions to catch on possible weird behaviours
         courseExecutions << [1, 2, 3, 4, 5]
+    }
+
+    def "Should fail to update a dashboard that doesn't exist"() {
+        when: "a dashboard is updated"
+        teacherDashboardService.updateTeacherDashboard(dashboardId)
+
+        then: "a TutorException is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.DASHBOARD_NOT_FOUND
+
+        where:
+        dashboardId << [0, 1, 100]
+    }
+
+    def "Update should update dashboard with latest student statistics"() {
+        given: "a course and course executions are created"
+        def course = new Course(COURSE_1_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+        def courseExecution = new CourseExecution(course, COURSE_1_ACRONYM, COURSE_1_ACADEMIC_TERM, Course.Type.TECNICO,
+                LOCAL_DATE_TODAY
+        )
+        courseExecutionRepository.save(courseExecution)
+
+        and: "a teacher is added to the course execution"
+        teacher.addCourse(courseExecution)
+
+        and: "a dashboard is created"
+        def teacherDashboardDto = teacherDashboardService.createTeacherDashboard(courseExecution.getId(), teacher.getId())
+
+        when: "a student is added to the course execution"
+        def student = new Student(USER_3_NAME, false);
+        student.addCourse(courseExecution)
+        userRepository.save(student)
+
+        and: "the dashboard is updated"
+        teacherDashboardService.updateTeacherDashboard(teacherDashboardDto.getId())
+
+        then: "the dashboard is updated with the latest student statistics"
+        teacherDashboardRepository.findAll().size() == 1;
+        teacherDashboardRepository.findAll().get(0).getStudentStats().size() == 1;
+        studentStatsRepository.findAll().size() == 1;
+        studentStatsRepository.findAll().get(0).getNumStudents() == 1;
+    }
+
+    def "When updating all teacher dashboard, student stats should update"() {
+        given: "a course and course executions are created"
+        def course = new Course(COURSE_1_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+        def courseExecution = new CourseExecution(course, COURSE_1_ACRONYM, COURSE_1_ACADEMIC_TERM, Course.Type.TECNICO,
+                LOCAL_DATE_TODAY
+        )
+        courseExecutionRepository.save(courseExecution)
+
+        and: "two teachers are added to the course execution"
+        teacher.addCourse(courseExecution)
+
+        def teacherB = new Teacher(USER_2_NAME, false);
+        teacherB.addCourse(courseExecution)
+        userRepository.save(teacherB)
+
+        and: "a dashboard is created"
+        // We are only creating one dashboard to test if the update all method updates existing dashboards
+        // but also creates new ones
+        teacherDashboardService.createTeacherDashboard(courseExecution.getId(), teacher.getId())
+
+        when: "a student is added to the course execution"
+        def student = new Student(USER_3_NAME, false);
+        student.addCourse(courseExecution)
+        userRepository.save(student)
+
+        and: "all teacher dashboards are updated"
+        teacherDashboardService.updateAllTeacherDashboards()
+
+        then: "dashboards are updated with the latest student statistics"
+        teacherDashboardRepository.findAll().size() == 2;
+        teacherDashboardRepository.findAll().get(0).getStudentStats().size() == 1;
+        teacherDashboardRepository.findAll().get(1).getStudentStats().size() == 1;
+        studentStatsRepository.findAll().size() == 2;
+        studentStatsRepository.findAll().get(0).getNumStudents() == 1;
+        studentStatsRepository.findAll().get(1).getNumStudents() == 1;
     }
 
     @TestConfiguration
