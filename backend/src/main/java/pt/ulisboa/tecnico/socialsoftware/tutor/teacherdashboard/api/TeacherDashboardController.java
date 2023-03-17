38d805ccd129a93f592e18e45683593261eed0e1
Adriana Nunes <adriana.nunes@tecnico.ulisboa.pt>
2023-03-17 11:53:51 +0000

    feat: REST service to updateAllTeacherDashboards Refs: #80

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
@@ -29,4 +30,10 @@ public class TeacherDashboardController {
         return teacherDashboardService.getTeacherDashboard(courseExecutionId, teacherId);
     }
 
+    @PutMapping("/teachers/dashboards/updateAll")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateAllTeacherDashboards() {
+        teacherDashboardService.updateAllTeacherDashboards();
+    }
+ 
 }
