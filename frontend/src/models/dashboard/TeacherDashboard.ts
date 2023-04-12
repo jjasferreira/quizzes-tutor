2ea4d6a244ae61109af8c162815371edc8059e45
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2023-04-12 16:52:12 +0100

    feat: Add teacher question model Refs: #92

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -2,6 +2,13 @@ import QuizStats from './QuizStats';
 export default class TeacherDashboard {
   id!: number;
   quizStats!: QuizStats[];
+  // exclamation mark tells typescript that these values can never be NULL
+  numberOfStudents!: number;
+
+  // questions
+  numAvailable!: number;
+  answeredQuestionsUnique!: number;
+  averageQuestionsAnswered!: number;
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
@@ -11,6 +18,12 @@ export default class TeacherDashboard {
           (quizStats: QuizStats) => new QuizStats(quizStats)
         );
       }
+      this.numberOfStudents = jsonObj.numberOfStudents;
+
+      // questions
+      this.numAvailable = jsonObj.numAvailable;
+      this.answeredQuestionsUnique = jsonObj.answeredQuestionsUnique;
+      this.averageQuestionsAnswered = jsonObj.averageQuestionsAnswered;
     }
   }
 }
