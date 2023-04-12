72c14186164a027843071ecab6ec5b7e4298ba56
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2023-04-12 20:31:11 +0100

    feat: start e2e test to teacherDashboard Refs: #99

---
+++ frontend/tests/e2e/specs/dashboard/teacherDashboard.js
@@ -0,0 +1,54 @@
+describe('Teacher Dashboard', () => {
+    // enter as demo teacher
+    beforeEach(() => {
+        cy.deleteQuestionsAndAnswers();
+        cy.demoTeacherLogin();
+        cy.get('[data-cy="dashboardMenuButton"]').click();
+    })
+
+    it('test only one execution (no stats)', () => {
+        let date = new Date();
+        cy.addTopicAndAssessment();
+
+        cy.createQuestion(
+          'Dashboard Question 1 ' + date,
+          'Question',
+          'Option',
+          'Option',
+          'ChooseThisWrong',
+          'Correct'
+        );
+
+        cy.get('[data-cy="Topics"]').click();
+        cy.contains('Software Architecture').click();
+
+        cy.createQuestion(
+          'Dashboard Question 2 ' + date,
+          'Question',
+          'Option',
+          'Option',
+          'ChooseThisWrong',
+          'Correct'
+        );
+
+        cy.createQuizzWith2Questions(
+          'Dashboard Title ' + date,
+          'Dashboard Question 1 ' + date,
+          'Dashboard Question 2 ' + date
+        );
+        //cy.get('[data-cy="dashboardMenuButton"]').click();
+        cy.contains('Logout').click();
+        cy.demoStudentLogin();
+        cy.solveQuizz('Dashboard Title ' + date, 2, 'ChooseThisWrong');
+        cy.contains('Logout').click();
+        cy.demoTeacherLogin();
+        cy.get('[data-cy="dashboardMenuButton"]').click();
+        cy.get('[data-cy="numStudents"]').should('have.text', 1);
+        cy.get('[data-cy="numAvailable"]').should('have.text', 2);
+        cy.get('[data-cy="answeredQuestionsUnique"]').should('have.text', 2);
+        cy.get('[data-cy="averageQuestionsAnswered"]').should('have.text', 2);
+
+
+        //TODO: verificar que não são mostrados gráficos
+    })
+})
