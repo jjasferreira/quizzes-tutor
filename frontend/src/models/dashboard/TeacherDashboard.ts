d387514026bccc4a8f6db5cae3770b2e49a6ce8b
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2023-04-14 14:06:53 +0100

    fix: removed unused variables

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -1,21 +1,10 @@
 import QuizStats from './QuizStats';
 import QuestionStats from './QuestionStats'
-import { Dictionary } from "vue-router/types/router";
 
 export default class TeacherDashboard {
   id!: number;
   quizStats!: QuizStats[];
   questionStats!: QuestionStats[];
-  // exclamation mark tells typescript that these values can never be NULL
-  numStudents!: number;
-
-  // questions
-  numAvailable!: number;
-  answeredQuestionsUnique!: number;
-  averageQuestionsAnswered!: number;
-
-  studentStats!: Array<Dictionary<number>>;
-
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
